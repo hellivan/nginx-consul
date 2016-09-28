e469a6a520a3df51e3c1da0e6fbcea37397d32b7
Ivan Hell <ivan.hell@innovaalp.com>
2016-09-28 17:29:58 +0200

    Fixewd path

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ CONSUL_URL=${CONSUL_HOST}:${CONSUL_PORT}
 echo Starting consul-template for consul @ ${CONSUL_URL}
 
 consul-template \
-    -consul="http://${CONSUL_URL}" \
+    -consul="${CONSUL_URL}" \
     -template="/etc/ctmpl/nginx.conf.ctmpl:/etc/nginx/nginx.conf" \
     -exec="/usr/sbin/nginx -g 'daemon off;'"
