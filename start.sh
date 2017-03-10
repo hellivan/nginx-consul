4cb73990cb64b6a4dd8538a91d0e1a43981d4599
Ivan Hell <ivan.hell@innovaalp.com>
2017-03-10 14:10:17 +0100

    fix: adjust start script to renamed paramters of consul-template

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ CONSUL_URL=${CONSUL_HOST}:${CONSUL_PORT}
 echo Starting consul-template for consul @ ${CONSUL_URL}
 
 consul-template \
-    -consul="${CONSUL_URL}" \
+    -consul-addr="${CONSUL_URL}" \
     -template="/etc/ctmpl/nginx.conf.ctmpl:/etc/nginx/nginx.conf" \
     -exec="/usr/sbin/nginx -g 'daemon off;'" \
     "$@"
